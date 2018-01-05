8c633e150574bebb10d7bf79c8f6f3d6a665883f
Katrina Brinson <kbrinso@gmail.com>
2018-01-05 01:12:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
   for (var i = 0; i < 25; i++ ){
     if (i >= 1){
-      break array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
